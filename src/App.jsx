dc8088765727ea1c6a8a4955f06e4f390385c6a2
RoneyVaLo <108905119+RoneyVaLo@users.noreply.github.com>
2026-01-13 15:00:56 -0600

    refactor App component; implement routing for Login, Test, and Summary pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Test from "./pages/Test";
+import Summary from "./pages/Summary";
+
 const App = () => {
-  return <div>Hola Mundo</div>;
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="test" element={<Test />} />
+        <Route path="summary" element={<Summary />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
